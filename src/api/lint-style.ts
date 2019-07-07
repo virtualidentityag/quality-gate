a46dfb2b9932339079ab737c3478f99c40d800dd
Tiago Marques <tiago.marques@virtual-identity.com>
2019-07-07 23:02:38 +0100

    Remove any

--- src/api/lint-style.ts
+++ src/api/lint-style.ts
@@ -1,13 +1,18 @@
-import { lint as Stylelint } from 'stylelint';
+import { lint, LinterOptions, LinterResult } from 'stylelint';
 
 import { Config } from './config';
 import { resolver } from './resolver';
 import { LintOptions, LintResult } from './types';
+
+interface CorrectedOptions extends LinterOptions {
+  allowEmptyInput: boolean;
+}
+
+const Stylelint = (options: Partial<CorrectedOptions>): Promise<LinterResult> => lint(options);
 
 export const lintStyle = (config: Config, options: LintOptions): Promise<LintResult['style']> => Stylelint({
   fix: options.fix,
   files: resolver(['.scss', '.css'], options.pattern),
   config: config.style,
   allowEmptyInput: true,
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-} as any);
+});
