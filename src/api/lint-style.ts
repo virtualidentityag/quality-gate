4bed54d21e218194b78e39941f4f02edb9ebfa8a
Tiago Marques <tiago.marques@virtual-identity.com>
2019-07-07 22:51:38 +0100

    Update dependencies

--- src/api/lint-style.ts
+++ src/api/lint-style.ts
@@ -8,4 +8,6 @@ export const lintStyle = (config: Config, options: LintOptions): Promise<LintRes
   fix: options.fix,
   files: resolver(['.scss', '.css'], options.pattern),
   config: config.style,
-});
+  allowEmptyInput: true,
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} as any);
