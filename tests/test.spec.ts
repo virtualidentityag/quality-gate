d5774a27f1e2479af6dd6ab8cfa6619d03ec7e0c
Tiago Marques <tiago.marques@virtual-identity.com>
2019-07-22 14:29:26 +0100

    Add --test-extension test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -8,15 +8,16 @@ describe('style linting', (): void => {
     {
       ...defaultOptions,
       tests: true,
+      testExtension: '.my-spec',
       pattern: `${PROJECTS_PATH}/test-fail`,
     },
     [
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [2, 9]: There should be no spaces inside this paren. (space-in-parens)`,
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [3, 1]: Expected indentation of 0 spaces but found 1. (@typescript-eslint/indent)`,
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [3, 2]: Opening curly brace does not appear on the same line as controlling statement. (brace-style)`,
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [4, 29]: Block must not be padded by blank lines. (padded-blocks)`,
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [6, 5]: Unexpected whitespace before property toBe. (no-whitespace-before-property)`,
-      `./${PROJECTS_PATH}/test-fail/index.spec.ts [6, 17]: Multiple spaces found before 'toBe'. (no-multi-spaces)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [2, 9]: There should be no spaces inside this paren. (space-in-parens)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [3, 1]: Expected indentation of 0 spaces but found 1. (@typescript-eslint/indent)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [3, 2]: Opening curly brace does not appear on the same line as controlling statement. (brace-style)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [4, 29]: Block must not be padded by blank lines. (padded-blocks)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [6, 5]: Unexpected whitespace before property toBe. (no-whitespace-before-property)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [6, 17]: Multiple spaces found before 'toBe'. (no-multi-spaces)`,
     ],
   );
 
