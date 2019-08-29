12e7db06e72cedb69c02ec64d79c806ce1f62635
Tiago Marques <tiago.marques@virtual-identity.com>
2019-08-29 11:22:03 +0100

    Update tests

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -15,6 +15,7 @@ describe('test linting', (): void => {
       `./${PROJECTS_PATH}/test-fail/wrongName.my-spec.ts: File path is error-prone. Use kebab-case or snake_case for folder names and files. (biotope-quality-gate/filenames)`,
       `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [2, 9]: There should be no spaces inside this paren. (space-in-parens)`,
       `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [3, 1]: Expected indentation of 0 spaces but found 1. (@typescript-eslint/indent)`,
+      `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [3, 1]: Expected indentation of 0 spaces but found 1. (indent)`,
       `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [3, 2]: Opening curly brace does not appear on the same line as controlling statement. (brace-style)`,
       `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [4, 29]: Block must not be padded by blank lines. (padded-blocks)`,
       `./${PROJECTS_PATH}/test-fail/index.my-spec.ts [6, 5]: Unexpected whitespace before property toBe. (no-whitespace-before-property)`,
