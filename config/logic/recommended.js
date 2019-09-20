3654122f7e1c9be7504deeda54c1c99b0bcb663a
Tiago Marques <tiago.marques@virtual-identity.com>
2019-09-20 12:16:25 +0100

    Adjust recommended rules for plugin upgrade

--- config/logic/recommended.js
+++ config/logic/recommended.js
@@ -4,9 +4,25 @@ module.exports = {
     'airbnb-base',
     'plugin:import/errors',
     'plugin:import/warnings',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
     'plugin:compat/recommended',
+  ],
+  // Needed because @typescript-eslint >= 2.0 does not check if file is typescript
+  overrides: [
+    {
+      // Regex for @typescript-eslint < 2.0 was /\.tsx?$/
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        // Modified Ts rules (to match AirBnB rules)
+        '@typescript-eslint/indent': ['error', 2],
+
+        // Disabled Ts rules (to let AirBnB rules deal with it)
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
   ],
   plugins: [
     'filenames',
@@ -16,12 +32,6 @@ module.exports = {
     'import/no-unresolved': 'off', // FIXME should be ignoring just '\\.(s?)css$', instead of being 'off'
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
-
-    // Modified Ts rules (to match AirBnB rules)
-    '@typescript-eslint/indent': ['error', 2],
-
-    // Disabled Ts rules (to let AirBnB rules deal with it)
-    '@typescript-eslint/no-var-requires': 'off',
 
     // eslint-plugin-filenames - Force kebab-case or snake_case file naming
     'filenames/match-regex': ['error', '^[a-z-_.]+$', true],
