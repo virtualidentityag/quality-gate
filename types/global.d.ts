8ae988f03d4919ebd9fce10c5d7904d0ad9e8fe6
Tiago Marques <tiago.marques@virtual-identity.com>
2019-09-20 14:25:19 +0100

    Minor type updates

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,14 +1,10 @@
 
-declare interface IndexObject<T> {
-  [key: string]: T;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any,@typescript-eslint/no-empty-interface
-declare interface IndexObjectAny extends IndexObject<any> {}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare type RecordAny = Record<string, any>;
 
 // FIXME: Typescript limitation on importing deconstructed json files
 // declare module '*.json' {
-//   const content: IndexObjectAny;
+//   const content: RecordAny;
 //   export = content;
 // }
 declare module '*.json';
