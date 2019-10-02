48a321f748e572a710174c6017c6772b7ce38307
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2019-10-02 11:00:24 +0100

    Task: Bump version (#108)
    * Minor packages update
    
    * Bump version
    
    * Remove duplicate props override

--- config/style/recommended.js
+++ config/style/recommended.js
@@ -10,13 +10,6 @@ module.exports = {
   ],
   rules: {
     'number-leading-zero': 'never',
-    'plugin/no-unsupported-browser-features': [true, {
-      severity: 'warning',
-      ignore: [
-        'calc', // due to IE9+ bug being neglectable
-      ],
-    }],
-    'declaration-block-no-duplicate-properties': true,
     'shorthand-property-no-redundant-values': true,
     'declaration-no-important': true,
     'property-no-vendor-prefix': true,
@@ -24,5 +17,11 @@ module.exports = {
     'string-quotes': 'single',
     'scss/dollar-variable-colon-space-after': 'always',
     'scss/dollar-variable-colon-space-before': 'never',
+    'plugin/no-unsupported-browser-features': [true, {
+      severity: 'warning',
+      ignore: [
+        'calc', // due to IE9+ bug being neglectable
+      ],
+    }],
   },
 };
