dbc035d2590d974366279dcc22ea44329ef4b48a
Tiago Marques <tiago.marques@virtual-identity.com>
2020-08-04 16:33:35 +0100

    chore: update rules for stencil

--- config/logic/recommended.js
+++ config/logic/recommended.js
@@ -30,14 +30,26 @@ module.exports = {
     'import/no-unresolved': 'off', // FIXME should be ignoring just '\\.(s?)css$', instead of being 'off'
     'import/prefer-default-export': 'off',
     'import/no-default-export': 'error',
+    'import/extensions': ['error', 'ignorePackages', { // add jsx and tsx extensions
+      js: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+      mjs: 'never',
+    }],
 
     // eslint-plugin-filenames - Force kebab-case or snake_case file naming
     'filenames/match-regex': ['error', '^[a-z-_.]+$', true],
 
-    // JSX for Stencil
+    // Stencil rules
     'react/react-in-jsx-scope': 'off', // "React" is not mandatory to be imported when JSX is present
     'react/no-unknown-property': ['error', { ignore: ['class'] }], // allow "class" attribute on JSX
     '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: 'h' }], // allow "h" unused variable (for Stencil)
+
+    // This rule cannot yet be disabled ONLY for specific packages like "@stencil/core"
+    // https://github.com/benmosher/eslint-plugin-import/issues/422
+    // 'import/no-extraneous-dependencies': ['error', { /* add exceptions here */ }],
+    'import/no-extraneous-dependencies': 'off',
   },
   env: {
     browser: true,
