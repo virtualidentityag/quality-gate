8927f54ab8f7b7796c29501fbaf9f414a6f86be4
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2020-08-10 08:59:02 +0100

    Feature: Storybook (#225)
    * feat: add rules for storybook

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -50,6 +50,9 @@ module.exports = {
     // https://github.com/benmosher/eslint-plugin-import/issues/422
     // 'import/no-extraneous-dependencies': ['error', { /* add exceptions here */ }],
     'import/no-extraneous-dependencies': 'off',
+
+    // Storybook
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
   },
   env: {
     browser: true,
